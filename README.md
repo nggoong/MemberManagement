cb3bba4ee57f0da32ea6e3b40b8045bf349e24f0
nggoong <nggoong@naver.com>
2021-11-03 22:31:43 +0900

    material-ui설치 및 테이블 디자인 적용✔

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 ## 리액트로 만드는 고객 관리 시스템✔
+### Skill stacks
+1. React
+> class component
+
+2. Material UI
+> `npm install @material-ui/core`
+
--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import logo from './logo.svg';
 import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import Table from '@material-ui/core/Table';
+import TableRow from '@material-ui/core/TableRow';
+
 
 
 const customers = [{
@@ -34,11 +40,25 @@ class App extends Component {
   render() {
     return (
       <div>
-        {customers.map(c=> {
-          return(
-            <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} job={c.job} gender={c.gender}/>
-          )
-        })}
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c=> {
+            return(
+              <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} job={c.job} gender={c.gender}/>
+            )
+          })}
+          </TableBody>
+        </Table>
       </div>
     );
   }
